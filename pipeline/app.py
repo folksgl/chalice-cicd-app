10b76cf51adeaa78ee02f16f901c844ccbffbf1b
folksgl <Gfolks14@gmail.com>
2020-12-11 10:23:21 -0500

    retrieve app_name from config file

--- pipeline/app.py
+++ pipeline/app.py
@@ -1,14 +1,20 @@
 """ AWS CDK Application for creating a PipelineStack """
 #!/usr/bin/env python3
 
+import json
 from aws_cdk import core
-
 from pipeline_stack import PipelineStack
 
 
 APP = core.App()
 
-APP_NAME = "chalice-app"
+with open("../app/.chalice/config.json") as config_file:
+    CONFIG = json.load(config_file)
+
+if "app_name" not in CONFIG:
+    print("Please configure 'app_name' in app/.chalice/config.json")
+
+APP_NAME = CONFIG.get("app_name")
 PIPELINE_STACK_NAME = f"{APP_NAME}-pipeline"
 
 PipelineStack(
