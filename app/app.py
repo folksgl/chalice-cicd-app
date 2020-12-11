10bb3d30ba4db092bc2370e4b8da1ae90d28b44e
folksgl <Gfolks14@gmail.com>
2020-12-11 11:33:26 -0500

    remove print from config loading

--- app/app.py
+++ app/app.py
@@ -4,7 +4,6 @@ from chalice import Chalice
 
 with open(".chalice/config.json") as config_file:
     CONFIG = json.load(config_file)
-    print(json.dumps(CONFIG))
 
 if "app_name" not in CONFIG:
     raise KeyError("No 'app_name' configured in app/.chalice/config.json")
