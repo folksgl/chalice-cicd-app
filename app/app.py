593b3715b630c45cebb7870054f71de3e3ff62e3
folksgl <Gfolks14@gmail.com>
2020-12-11 09:40:47 -0500

    correct pylint warnings

--- app/app.py
+++ app/app.py
@@ -1,30 +1,19 @@
+""" Sample Chalice "hello world" application """
 from chalice import Chalice
 
-app = Chalice(app_name="app")
+APP = Chalice(app_name="app")
 
 
-@app.route("/")
+@APP.route("/")
 def index():
+    """ Index page for the hello world api """
     return {"hello": "world"}
 
 
-@app.route("/hello/{name}")
+@APP.route("/hello/{name}")
 def hello_name(name):
-    # '/hello/james' -> {"hello": "james"}
+    """ Sample route for returning names """
     return {"hello": name}
 
 
-# The view function above will return {"hello": "world"}
-# whenever you make an HTTP GET request to '/'.
-#
-# Here are a few more examples:
-#
-# @app.route('/users', methods=['POST'])
-# def create_user():
-#     # This is the JSON body the user sent in their POST request.
-#     user_as_json = app.current_request.json_body
-#     # We'll echo the json body back to the user in a 'user' key.
-#     return {'user': user_as_json}
-#
-# See the README documentation for more examples.
-#
+# See https://aws.github.io/chalice/index.html for more examples
